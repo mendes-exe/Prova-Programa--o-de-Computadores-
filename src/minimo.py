52eb6f5a6ead697e757ce69a0a8d5f5519996bd2
Lucas Mendes <lucas.mendes.sep@gmail.com>
2025-05-12 19:01:04 -0300

    subindo 1 vez

--- src/minimo.py
+++ src/minimo.py
@@ -2,6 +2,10 @@ from .ler_arquivo_csv import ler_csv
 import pandas as pd
 
 def calc_minimo():
-    minimo = (ler_csv())['vendas'].min()
-    print("O minimo é é: R$" + str(minimo))
+    min_valor = (ler_csv())['vendas'].min()
+    mes_min = ler_csv()[ler_csv()['vendas'] == min_valor].iloc[0]['mes']
+    # print("O minimo é é: R$" + str(min_valor) + "no mes de: " + str(mes_min))
+    minimo = (f"O minimo é: R$ {str(min_valor)} no mes de: + {str(mes_min)}")
+    print(minimo)
     return minimo
+
